4ba5d1447184cbca0ae20a14dd543a090c4aac65
tungstennn <aqorane77@gmail.com>
2025-01-21 01:16:50 +0000

    Extraction

---
+++ py_script.py
@@ -0,0 +1,35 @@
+import requests
+import pandas as pd
+
+# Replace with your API key
+API_KEY = "a33dcb921c7e47938c2c3f9fc1dede2f"
+BTC_symbol = "BTC/GBP"
+ETH_symbol = "ETH/GBP"
+
+interval = "30min"     # Hourly data
+
+
+# Function to get crypto data
+def get_crypto_data(symbol, interval):
+    # API Endpoint
+    url = f"https://api.twelvedata.com/time_series?symbol={symbol}&interval={interval}&apikey={API_KEY}"
+
+    # Fetch data
+    response = requests.get(url)
+    data = response.json()
+
+    # Check for errors
+    if "values" in data:
+        df = pd.DataFrame(data["values"])
+        df["datetime"] = pd.to_datetime(df["datetime"])
+        print(symbol)
+        print(df.head())
+        print('--------------------------------------------------------------')
+    else:
+        print("Error:", data.get("message", "Unknown error"))
+        print('--------------------------------------------------------------')
+
+
+# Get data
+get_crypto_data(BTC_symbol, interval)
+get_crypto_data(ETH_symbol, interval)
